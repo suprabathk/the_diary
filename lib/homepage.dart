00384654bbd045cd8284a237a6334cdf3b90d173
Suprabath kondapally <suprabathkondapally@gmail.com>
2023-03-27 16:54:42 +0530

    final version

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:the_diary/add_file.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -14,23 +15,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  void getTabData() async {
-    await firestore
-        .collection('assignment')
-        .get()
-        .then((QuerySnapshot? querySnapshot) {
-      for (var doc in querySnapshot!.docs) {
-        print(doc.data());
-      }
-    });
-  }
-
-  @override
-  void initState() {
-    print("executing");
-    getTabData();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,89 +63,164 @@ class _HomePageState extends State<HomePage> {
                   final messages = snapshot.data?.docs;
                   for (var message in messages!) {
                     arr.add(
+                      //   Padding(
+                      //     padding: const EdgeInsets.all(18),
+                      //     child: Container(
+                      //       decoration: BoxDecoration(
+                      //         color: const Color.fromARGB(255, 213, 173, 235),
+                      //         borderRadius: const BorderRadius.all(
+                      //           Radius.circular(
+                      //             20,
+                      //           ),
+                      //         ),
+                      //         boxShadow: [
+                      //           BoxShadow(
+                      //             color: Colors.purple.withOpacity(0.5),
+                      //             spreadRadius: 1,
+                      //             blurRadius: 2,
+                      //           ),
+                      //         ],
+                      //       ),
+                      //       child: Padding(
+                      //         padding: const EdgeInsets.only(
+                      //             top: 8.0, left: 16, bottom: 8.0, right: 8.0),
+                      //         child: Row(
+                      //           children: [
+                      //             const CircleAvatar(
+                      //                 radius: 28,
+                      //                 backgroundImage: NetworkImage(
+                      //                     'https://cdn-icons-png.flaticon.com/512/1154/1154987.png?w=740&t=st=1679910352~exp=1679910952~hmac=1a103f049719d6d7c85da5ff5f2868fba8ae1b1319a8cd7cbf880924cb31877f')),
+                      //             Padding(
+                      //               padding: const EdgeInsets.only(left: 18.0),
+                      //               child: Column(
+                      //                 crossAxisAlignment:
+                      //                     CrossAxisAlignment.start,
+                      //                 children: [
+                      //                   Row(
+                      //                     children: [
+                      //                       Text(
+                      //                         '${message.get('title')}',
+                      //                         style: const TextStyle(
+                      //                           color: Colors.black,
+                      //                           fontWeight: FontWeight.bold,
+                      //                           fontSize: 24.0,
+                      //                         ),
+                      //                       ),
+                      //                       SizedBox(width: 50),
+                      //                       Text(
+                      //                         'Class: ${message.get('class')}',
+                      //                         style: const TextStyle(
+                      //                           color: Colors.black,
+                      //                           fontWeight: FontWeight.bold,
+                      //                           fontSize: 18.0,
+                      //                         ),
+                      //                       ),
+                      //                     ],
+                      //                   ),
+                      //                   const SizedBox(
+                      //                     height: 6,
+                      //                   ),
+                      //                   Text(
+                      //                     '${message.get('description')}',
+                      //                     style: const TextStyle(
+                      //                       color: Colors.black,
+                      //                       fontWeight: FontWeight.normal,
+                      //                       fontSize: 18.0,
+                      //                     ),
+                      //                   ),
+                      //                   Padding(
+                      //                     padding:
+                      //                         const EdgeInsets.only(top: 8.0),
+                      //                     child: Text(
+                      //                       '${message.get('due_date').toString().substring(0, 10)}',
+                      //                       style: const TextStyle(
+                      //                         color: Colors.black,
+                      //                         fontWeight: FontWeight.normal,
+                      //                         fontSize: 18.0,
+                      //                       ),
+                      //                     ),
+                      //                   ),
+                      //                 ],
+                      //               ),
+                      //             ),
+                      //           ],
+                      //         ),
+                      //       ),
+                      //     ),
+                      //   ),
                       Padding(
-                        padding: const EdgeInsets.all(18),
+                        padding: const EdgeInsets.all(12),
                         child: Container(
                           decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 213, 173, 235),
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(
-                                20,
-                              ),
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.purple.withOpacity(0.5),
-                                spreadRadius: 1,
-                                blurRadius: 2,
-                              ),
-                            ],
+                            color: const Color(0xFFD2D4FF),
+                            borderRadius: BorderRadius.circular(15),
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                                top: 8.0, left: 16, bottom: 8.0, right: 8.0),
-                            child: Row(
-                              children: [
-                                const CircleAvatar(
-                                    radius: 28,
-                                    backgroundImage: NetworkImage(
-                                        'https://cdn-icons-png.flaticon.com/512/1154/1154987.png?w=740&t=st=1679910352~exp=1679910952~hmac=1a103f049719d6d7c85da5ff5f2868fba8ae1b1319a8cd7cbf880924cb31877f')),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 18.0),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Text(
-                                            '${message.get('title')}',
-                                            style: const TextStyle(
-                                              color: Colors.black,
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 24.0,
-                                            ),
-                                          ),
-                                          SizedBox(width: 50),
-                                          Text(
-                                            'Class: ${message.get('class')}',
-                                            style: const TextStyle(
-                                              color: Colors.black,
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 18.0,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      const SizedBox(
-                                        height: 6,
-                                      ),
-                                      Text(
-                                        '${message.get('description')}',
-                                        style: const TextStyle(
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.normal,
-                                          fontSize: 18.0,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 12.0),
+                          margin: const EdgeInsets.symmetric(vertical: 4),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Row(
+                                  children: [
+                                    const CircleAvatar(
+                                        radius: 28,
+                                        backgroundImage: NetworkImage(
+                                            'https://cdn-icons-png.flaticon.com/512/1154/1154987.png?w=740&t=st=1679910352~exp=1679910952~hmac=1a103f049719d6d7c85da5ff5f2868fba8ae1b1319a8cd7cbf880924cb31877f')),
+                                    const SizedBox(
+                                      width: 20,
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text(
+                                          message.get('title'),
+                                          style: GoogleFonts.publicSans(
+                                              textStyle: const TextStyle(
+                                                  fontSize: 28)),
                                         ),
-                                      ),
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(top: 8.0),
-                                        child: Text(
-                                          '${message.get('due_date').toString().substring(0, 10)}',
-                                          style: const TextStyle(
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.normal,
-                                            fontSize: 18.0,
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(top: 4.0),
+                                          child: Text(
+                                            message.get('description'),
+                                            style: GoogleFonts.publicSans(
+                                                textStyle: const TextStyle(
+                                                    fontSize: 16)),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 8.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      Expanded(
+                                          child: Text(
+                                        "Due Date: ${message.get('due_date').toString().substring(0, 10)}",
+                                        style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w700),
+                                      )),
+                                      SizedBox(
+                                        width: 120,
+                                        child: Padding(
+                                          padding:
+                                              const EdgeInsets.only(right: 8),
+                                          child: Text(
+                                            "Class: ${message.get("class")}",
+                                            style:
+                                                const TextStyle(fontSize: 18),
                                           ),
                                         ),
                                       ),
                                     ],
                                   ),
                                 ),
-                              ],
-                            ),
-                          ),
+                              ]),
                         ),
                       ),
                     );
